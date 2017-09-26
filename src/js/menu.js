b62e261fdd4c1483083fd93c66bd521a81f6e57d
Sergey Yushmanov <s.yushmanov777@gmail.com>
2017-09-26 23:48:59 +0700

    Add scroll

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,11 +1,19 @@
-$(document).ready(function() {
-  //Для липкого меню
-  $(window).scroll(function() {
-    if ($(this).scrollTop() >= 90) {
-      $(".headermenu").addClass("fix");
-    } else {
-      $(".headermenu").removeClass("fix");
-    }
+$(function() {
+  $(document).ready(function() {
+    //Для липкого меню
+    $(window).scroll(function() {
+      if ($(this).scrollTop() >= 90) {
+        $(".headermenu").addClass("fix");
+      } else {
+        $(".headermenu").removeClass("fix");
+      }
+    });
+    //конец липкого меню
   });
-  //конец липкого меню
+  $("body").append('<div class="top"><i class="fa fa-angle-double-up">');
+
+      $("body").on("click", ".top", function() {
+        $("html, body").animate({scrollTop: 0}, "slow");
+
+      });
 });
